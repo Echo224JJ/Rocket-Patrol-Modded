b38c77ed0ee201e835aabd4f70635d4ebbdd8899
Joshua Jung <jokejung@ucsc.edu>
2020-04-17 15:33:05 -0700

    Initial commit

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,23 @@
+//Spaship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);   // add an object to existing, displayList, updateList
+        this.points = pointValue;
+    }
+
+    update() {
+        // move spaceship left
+        this.x -= game.settings.spaceshipSpeed;
+
+        //wraparound screen bounds
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.x = game.config.width;
+    }
+}
